ae44e42ef6b84eec134c4779de0c4648042b2bff
takagi <takagi@tagbangers.co.jp>
2025-03-05 18:03:25 +0900

    TAGTAKAGI-30 ヘッダーのドラフト作成

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,25 @@
 import { Provider } from "@/components/ui/provider";
 import "./globals.css";
+import { Header } from "@/components/blocks/Header";
+import { css } from "../../styled-system/css";
+
+//スマホサイズで開発
+const layoutStyle = css({
+	maxWidth: "480px",
+	margin: "0 auto", // 中央寄せ
+	minHeight: "100vh", // 画面全体の高さを確保
+	backgroundColor: "stone.50", // あとでかえる
+});
 
 export default function RootLayout(props: { children: React.ReactNode }) {
 	const { children } = props;
 	return (
 		<html lang="ja" suppressHydrationWarning>
-			<body>
-				<Provider>{children}</Provider>
+			<body className={layoutStyle}>
+				<Provider>
+					<Header />
+					{children}
+				</Provider>
 			</body>
 		</html>
 	);
---
+++ src/components/blocks/Header/styles.ts
@@ -0,0 +1,22 @@
+import { css } from "../../../../styled-system/css";
+
+export const headerStyles = css({
+  display: "flex",
+  justifyContent: "space-between",
+  padding: "10px 20px",
+  backgroundColor: "stone.100",
+  height: "60px",
+});
+
+export const ImageSize = css({
+  width: "50px",
+  height: "50px",
+  borderRadius: "50%",
+  position: "relative",
+});
+
+export const titleStyle = css({
+  color: "black.400", //（to do 色かえる）
+  fontSize: "24px",
+  fontWeight: "bold",
+});

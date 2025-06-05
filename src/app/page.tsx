a4c7d98b81b029fd7bbd8924b4c231e58d7650cb
aki05162525 <akihiro05162525@gmail.com>
2025-06-05 23:04:42 +0900

    無限レンダリングの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
-import { Button, Flex, Heading } from "@chakra-ui/react";
-import { Plus } from "lucide-react";
+import { HomeScreen } from "@/components/screens/home";
 
 export default function Home() {
   return (
     <div>
-      <Flex justify="space-between">
-        <Heading size="3xl" fontWeight="bold">
-          リーグ一覧
-        </Heading>
-        <Button asChild variant="surface" rounded="full">
-          <a href="./leagues/new">
-            <Plus />
-            リーグ追加
-          </a>
-        </Button>
-      </Flex>
+      <HomeScreen />
     </div>
   );
 }
--- src/components/blocks/league-list-header/index.tsx
+++ src/components/blocks/league-list-header/index.tsx
@@ -1,5 +1,18 @@
-import { HomeScreen } from "@/components/screens/home";
+import { Button, Flex, Heading } from "@chakra-ui/react";
+import { Plus } from "lucide-react";
 
 export const LeagueListHeader = () => {
-  return <HomeScreen />;
+  return (
+    <Flex justify="space-between">
+      <Heading size="3xl" fontWeight="bold">
+        リーグ一覧
+      </Heading>
+      <Button asChild variant="surface" rounded="full">
+        <a href="./leagues/new">
+          <Plus />
+          リーグ追加
+        </a>
+      </Button>
+    </Flex>
+  );
 };

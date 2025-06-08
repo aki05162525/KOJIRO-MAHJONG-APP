1e72c1d48dada7c0b55ec6931cf6b646c1c9a4ba
aki05162525 <akihiro05162525@gmail.com>
2025-06-08 21:25:51 +0900

    rtkの環境構築

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -1,19 +1,19 @@
 "use client";
 import { useRef } from "react";
+import type React from "react";
 import { Provider } from "react-redux";
 import { type AppStore, makeStore } from "../infla/store";
-import type React from "react";
 
 export default function StoreProvider({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore>(undefined);
-  if (!storeRef.current) {
-    // Create the store instance the first time this renders
-    storeRef.current = makeStore();
-  }
+	const storeRef = useRef<AppStore>(undefined);
+	if (!storeRef.current) {
+		// Create the store instance the first time this renders
+		storeRef.current = makeStore();
+	}
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
+	return <Provider store={storeRef.current}>{children}</Provider>;
 }
